3605e125ca0118fb3ce658deb28a01922c8fd220
Maria Luisa <Maluworkq@gmail.com>
2025-10-22 20:34:27 -0300

    Add files via upload

---
+++ PAM - APS/Atv_Pam/js_Array.js
@@ -0,0 +1,14 @@
+function SomaArray(arr){
+    let total = 0;
+    for  (let i = 0; i < arr.length; i++){
+        total +=arr[1];
+    }
+    return total
+}
+
+const Lista_NUmeros = [10,20,30,45,55];
+
+console.log("")
+console.log("Function Array")
+console.log(SomaArray(Lista_NUmeros));
+
---
+++ PAM - APS/Atv_Pam/js_Normal.js
@@ -0,0 +1,7 @@
+console.log("Function Normal")
+
+function Soma(a,b){
+    return a+b;
+}
+console.log(Soma(4,5));
+

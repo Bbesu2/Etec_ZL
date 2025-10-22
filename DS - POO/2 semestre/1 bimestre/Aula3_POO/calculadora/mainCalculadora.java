811f4e574f205ebb7d0bce791527a860d243b146
Maria Luisa <Maluworkq@gmail.com>
2025-10-22 10:19:21 -0300

    Add files via upload

---
+++ DS - POO/2 semestre/1 bimestre/Aula3_POO/calculadora/mainCalculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Admin
+ */
+public class mainCalculadora {
+    
+    public static void main(String[] args){
+    
+    Calculadora calculadora = new Calculadora();
+    
+    calculadora.soma();
+    calculadora.subtracao();
+    calculadora.divisao();
+    calculadora.multiplicacao();
+        
+    }
+}

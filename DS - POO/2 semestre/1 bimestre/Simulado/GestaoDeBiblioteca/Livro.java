9419a4ed96c4f37e50d62347914975608161be85
Maria Luisa <Maluworkq@gmail.com>
2025-10-22 10:26:04 -0300

    Add files via upload

---
+++ DS - POO/2 semestre/1 bimestre/Simulado/GestaoDeBiblioteca/Livro.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestaoDeBiblioteca;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+}
+
